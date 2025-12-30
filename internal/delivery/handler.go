4347becb992c744622f95ecbcd1be4127e0c60f9
Ahmad Khan <nuzulakhanahmadfirdausi@gmail.com>
2025-12-30 12:54:50 +0700

    fix: remove MaxAge from CORS configuration in handler

--- internal/delivery/handler.go
+++ internal/delivery/handler.go
@@ -25,8 +25,7 @@ func NewHandler(dataUc *usecase.DataUsecase, authUc *usecase.AuthUsecase, newsUc
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-Requested-With"},
 		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
+		AllowCredentials: true
 	}))
 
 	dataHandler := NewDataHandler(dataUc, hub)
